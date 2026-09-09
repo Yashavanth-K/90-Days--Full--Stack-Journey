5a575bbceb1d22ba45edb1535a5bfd25e6b85768
K Yashavanth <yashyashavanthk8@gmail.com>
2026-09-09 19:21:23 +0530

    Add Day33ofLearning.js with personal details
    This is my first base of learning of javascript  😎⭐

---
+++ Day33ofLearning.js
@@ -0,0 +1,13 @@
+let name = "Yashavanth";
+let age = 19;
+let isStudent = true;
+let favoritelanguage = "HTML";
+let islikecoding = true;
+
+document.getElementById("name").textContent = name;
+document.getElementById("name").textContent = name;
+document.getElementById("age").textContent = age;
+document.getElementById("isStudent").textContent = isStudent;
+document.getElementById("favoritelanguage").textContent = favoritelanguage;
+document.getElementById("islikecoding").textContent = islikecoding;
+
