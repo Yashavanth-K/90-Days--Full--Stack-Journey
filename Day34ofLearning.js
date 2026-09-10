6729878c12ed435d95c8a1cd7ca385eb8e3b3907
K Yashavanth <yashyashavanthk8@gmail.com>
2026-09-10 20:05:57 +0530

    Add interactive introduction web page functionality
    It is my day 34 javascript code 😎⭐

---
+++ Day34ofLearning.js
@@ -0,0 +1,9 @@
+alert("🌟 Welcome to Interactive Introduction Web Page 🌟");
+
+
+ 
+document.getElementById("startButton").onclick = function() {
+  let variableName = prompt("What is Your Name");
+  let variableage = prompt("Your age");
+  let likesCoding = confirm("Do you like coding?"); 
+};
