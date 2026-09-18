3b80ba9aafeab9e5a2c8190ffc59d246b95cd2b9
K Yashavanth <yashyashavanthk8@gmail.com>
2026-09-18 18:54:34 +0530

    Add names and animals arrays with DOM updates

---
+++ Day37ofLearning.js
@@ -0,0 +1,14 @@
+let names=["Yashavanth" , "Kotresh" , "Saraswathi"];
+
+names[1]= "Rhaul";
+names[2]= "Kotresh"
+
+document.getElementById("sum").textContent = names;
+
+document.getElementById("length").textContent = names.length;
+
+let animals=["Lion" , "Tiger" , "Elephant" , "Bear"]
+
+document.getElementById("forest").textContent = animals;
+
+document.getElementById("total").textContent=animals.length;
